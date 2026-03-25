cc44f1ee7e612dab54dfdadb8696936a14cd75f2
Ulpi Mahesh <maheshanandulpi@gmail.com>
2026-03-25 13:17:53 -0500

    error handling

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,23 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import os
 
 def make_driver(headless=True):
     options = Options()
 
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-extensions')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
 
     if headless:
-        options.add_argument('--headless=new')
+        options.add_argument("--headless=new")
 
-    # 🔥 ADD THIS FOR RENDER
-    if os.environ.get("RENDER"):
-        options.binary_location = "/usr/bin/chromium"
-        return webdriver.Chrome(options=options)
+    # 🔥 CRITICAL FOR RENDER
+    options.binary_location = "/usr/bin/chromium"
 
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    return webdriver.Chrome(options=options)
